a5c53ad2005ce2a39dcf2c85e982058a3dd86695
Affan-Zulfiqar <affan.zulfiqar313@gmail.com>
2023-03-03 16:36:13 +0000

    testing

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import './style.css'
+import HeaderSec from './pages/HeaderSec.vue'
+import homeSec from './pages/homeSec.vue'
+import footerSec from './pages/footerSec.vue'
+
+</script>
+
+<template>
+
+<HeaderSec></HeaderSec>
+<homeSec></homeSec>
+<routerview></routerview>
+<footerSec></footerSec>
+
+
+</template>
+
+
+
+
+
---
+++ src/pages/aboutSec.vue
@@ -0,0 +1,60 @@
+<div class=" grid grid-cols-5 max-w-7xl mx-auto gap-3 py-20  px-3 bg-white">
+    <div class="col-span-2">
+        <div class="img relative">
+           <img class="ml-auto mt-5" src="images/Turn-your-utility-bill-into-a-savings-account.-1.png" alt="">
+
+           <div class="img-box-2 absolute top-14 left-6 bsColor w-56 text-center rounded-xl px-4 py-8">
+            <img class="w-14 mx-auto mb-4" src="images/solar-savings.png" alt="">
+             <p class="text-white mx-auto text-base">Customers will, on average, recoup their solar investment within 6-9 years.</p>
+           </div>
+
+        </div>
+        
+    </div>
+
+    <div class="col-span-3 pl-16 pr-12">
+       <div>
+        <h2 class="text-clr1 text-4xl font-bold mb-4">Complete Solar Power System for Your Home</h2>
+        <p class="text-clr1 bhColor w-fit px-5 py-1 text-base font-bold my-4">Turn your utility bill into a savings account.</p>
+        <p class="text-clr1 text-lg font-bold  leadin-6 my-3">For anyone who’s ever been sick of paying endless bills every month – we’re here for you.</p>
+        <p class="text-clr1 text-base font-medium mt-5 mb-7">Going solar creates your own power, which is pretty cool. But it also gives you back your personal power, your money, 
+          those hundreds of dollars you were going to pay to the power company every month and get nothing in return.</p>
+
+
+          <ul class="text-clr1 my-4 ">
+            <div class="flex  items-center text-lg mb-4">
+              
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-3">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd"></path>
+              </svg>
+              
+                <p class="font-bold">Save money? Check.</p>
+            </div>
+            <div class="flex justify between items-center text-lg mb-4">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-3">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd"></path>
+              </svg>
+                <p class="font-bold">Make Money? Check.</p>
+            </div>
+            <div class="flex justify between items-center text-lg mb-4">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 mr-3">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd"></path>
+              </svg>
+                <p class="font-bold">Easy? Check.</p>
+            </div>
+            
+          </ul>
+          <p class="text-black my-7 text-lg font-normal">In a world of complicated financial decisions, we’re simple.</p>
+
+      <div class="solar-button bg-green w-fit font-bold transition duration-300  mt-20">
+          <a href="#" class=" flex justify between items-center text-white px-6 py-5">
+           
+            Get My FREE Solar Quote<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 ml-2 ">
+              <path fill-rule="evenodd" d="M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z" clip-rule="evenodd"></path>
+            </svg>
+            
+          </a>
+        </div>
+       </div>
+    </div>
+  </div>
